8b1a1718b7749cead3b7665dee68fb87604a94f8
Robert Kulyassa <robert.kulyassa@qnective.com>
2019-12-03 15:12:08 +0100

    + checking download via IPv6 too.

--- check_sites_ipv6.sh
+++ check_sites_ipv6.sh
@@ -7,6 +7,13 @@ has_ipv6_address() {
     fi
 }
 
+page_download_on_ipv6() {
+    if wget --quiet -6 http://$1 -O /dev/null
+        then return 0
+        else return 1
+    fi
+}
+
 
 if [ -z "$1" ] || [ ! -f "toppages/$1" ]; then
     echo "Missing argument or wrong filename" >&2
@@ -21,15 +28,25 @@ if [ -f results/${PAGES_FILE} ]; then
 fi
 
 i=0
-n=0
+ipv6_address_n=0
+ipv6_download_n=0
+
 for PAGE in $(cat toppages/${PAGES_FILE}); do
     i=$[i+1]
-    if has_ipv6_address $PAGE
-        then ipv6_status=yes; n=$[n+1]
-        else ipv6_status=no
+    ipv6_address=no
+    ipv6_download=no
+    if has_ipv6_address $PAGE; then
+        ipv6_address=yes
+        ipv6_address_n=$[ipv6_address_n+1]
+        if page_download_on_ipv6 $PAGE; then
+            ipv6_download=yes
+            ipv6_download_n=$[ipv6_download_n+1]
+        fi
     fi
-    echo "$i;$PAGE;$ipv6_status" >> results/${PAGES_FILE}
+    echo "$i;$PAGE;$ipv6_address;$ipv6_download" >> results/${PAGES_FILE}
 done
 
-echo "$i/$n"
+echo "All sites:                        $i"
+echo "Sites with IPv6 address:          $ipv6_address_n"
+echo "Sites with working IPv6 download: $ipv6_download_n"
 
